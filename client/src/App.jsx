47227033e219fcd1837f9ede4dab7fe8845fd617
Naoman <ntabassam2@gmail.com>
2026-04-15 12:44:12 +0100

    pushing up to allow whitney to start her work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/dashboard">Go to Dashboard</Link>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/aisuggestions" element={<Aisuggestions />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/Dashboard.jsx
@@ -0,0 +1,21 @@
+import './Dashboard.css'
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            
+            <h1 className="title">Dashboard Menu</h1>
+
+            <div className="box-container">
+                <div className="box"></div>
+                <div className="box"></div>
+            </div>
+
+            <div className="interest-box"></div>
+
+
+        </div>
+    );
+}
+
+export default Dashboard;
