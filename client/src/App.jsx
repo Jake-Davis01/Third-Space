f2417b0ce1da0844a07e3e78f93280fa6af69e6d
Jake Davis <44555512+Jake-Davis01@users.noreply.github.com>
2026-04-15 12:09:34 +0100

    Added in basic nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 
+import { NavBar } from './navbar/navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
+      <NavBar />
       <h1>Hello World!</h1>
     </>
   )
