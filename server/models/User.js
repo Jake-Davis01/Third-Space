c61216b36b926e005fb1504109abfa9034194740
CharlotteBeecher <beechercharlotte@gmail.com>
2026-04-17 08:58:45 +0100

    updated model to store user interests correctly

--- server/models/User.js
+++ server/models/User.js
@@ -30,17 +30,29 @@ class User {
         return new User(response.rows[0]);
     }
 
-    static async create(data) {
-        const { first_name, last_name, email, password, user_interests, meetup_preference, office_location } = data;
-        const response = await db.query(
-            `INSERT INTO users (first_name, last_name, email, password_hash, user_interests, meetup_preference, office_location) 
-             VALUES ($1, $2, $3, $4, $5, $6, $7) 
-             RETURNING id`,
-            [first_name, last_name, email, password, user_interests, meetup_preference, office_location]
+static async create(data) {
+    const { first_name, last_name, email, password, user_interests, meetup_preference, office_location } = data;
+
+    // Insert the user (no user_interests column needed here anymore)
+    const response = await db.query(
+        `INSERT INTO users (first_name, last_name, email, password_hash, meetup_preference, office_location) 
+         VALUES ($1, $2, $3, $4, $5, $6) 
+         RETURNING id`,
+        [first_name, last_name, email, password, meetup_preference, office_location]
+    );
+
+    const newId = response.rows[0].id;
+
+    // Insert each interest into the junction table
+    // user_interests comes in as an array e.g. ["Running", "Film", "Reading"]
+    for (const interest of user_interests) {
+        await db.query(
+            `INSERT INTO user_interests (user_email, interest_name) VALUES ($1, $2)`,
+            [email, interest]
         );
-        const newId = response.rows[0].id;
-        return await User.getOneById(newId);
     }
-}
+
+    return await User.getOneById(newId);
+}}
 
 module.exports = User;
